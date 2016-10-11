7e2f416a1b9db31c1562d958518711949ab90f19
Cess <cessmbuguar@gmail.com>
2016-10-11 08:14:51 +0300

    use of regular expression sinpet

---
+++ regex.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+get %r{/(sp|gr)eedy} do
+"You got caught in very greedy route!!!"
+end
+get '/speedy'do
+  "No one calls me hehehe y? Seriously why? :("
+end
+get '/greedy' do 
+  "No one calls me either :( :("
+end
+
