2e59da8ead8857e7029d2e1e6b973891a6eb7000
Cess <cessmbuguar@gmail.com>
2016-10-12 10:50:51 +0300

    sample for inline template

---
+++ inline.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+get '/index' do
+  erb :index
+end
+
+__END__
+
+@@index
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+mine
+</title>
+</head>
+<h1> 
+it worked
+</h1>
+<p> Or not </p>
+</html
