75715df72d4e40558b98c122fd68f10e30135325
Cess <cessmbuguar@gmail.com>
2016-10-15 23:58:38 +0300

    sending an attachment to client

---
+++ attach.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+before do
+  content_type :txt
+end
+
+get'/attachment' do
+  attachment 'name_your_attachment.txt'
+"Here's what will be sent downstream, in an attachment called ''name_your_attachment.txt'"
+end
