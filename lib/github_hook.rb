13c4efade1c572b4181c58e25c971eae0c0dfa9d
Cess <cessmbuguar@gmail.com>
2016-10-21 21:20:40 +0300

    pulling changes using autopull

--- lib/github_hook.rb
+++ lib/github_hook.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
 require 'time'
 class GithubHook < Sinatra::Base
+  set(:autopull) { production? }
+
   post '/update' do
     app.settings.reset!
     load app.settings.app_file
 
     content_type :txt
-    "ok"
+    if settings.autopull?
+      'git pull 2>&1'
+    else
+      "ok"
+    end
   end
 end
